5b8657e0c8b33d6e5265b799bf68ed6cf366c9cd
Audrey Lee <144634744+audrey06lee05@users.noreply.github.com>
2024-09-20 07:34:39 +0800

    Time: 56 ms (33.57%), Space: 16.7 MB (9.33%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.py
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.py
@@ -1,6 +1,6 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        
+        """
         # create a list to store total money in each account:
         each_account = []
 
@@ -22,4 +22,33 @@ class Solution:
         max_account =  max(each_account)
 
         return max_account
+         """   
+
+        # set constraint for the while loop 
+        i = 0 
+        j = 0
+
+        # create a list to store total money in each account:
+        each_account = []
+
+        counter = 0 # counter for storing money in each account
+
+        # loop through accounts
+        while i < len(accounts):
             
+            # access elements of each account
+            if j < len(accounts[i]):
+                counter += accounts[i][j] # add money to counter
+                j+=1 # next element
+            else: 
+                each_account.append(counter)
+                i +=1 # move on to next account
+                j = 0 # reset element to first element of next account
+                counter = 0 # reset counter
+        
+
+        # find maximum in each_account
+        max_account =  max(each_account)
+
+        return max_account
+        
