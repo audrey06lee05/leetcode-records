49fe3a29a11d012a1e2e48029c71a34943ec2f69
Audrey Lee <144634744+audrey06lee05@users.noreply.github.com>
2024-10-05 00:47:20 +0100

    Time: 43 ms (48.81%), Space: 16.9 MB (33.92%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -1,5 +1,21 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        
+        sum_list = [] # store sum of each element
+
+        # loop through each element of nums:
+        for i in range(len(nums)):
+            # first index element can append to sum_list directly
+            if i == 0:
+                sum_list.append(nums[i])
+            else:
+                target = nums[i] + sum_list[i-1] # add correct sum_list element to nums[i]
+                sum_list.append(target) # add target to sum_list
+
+        return sum_list
+
+
+    """
         # create a list to store the sum of each element
         sum_list = []
 
@@ -12,7 +28,7 @@ class Solution:
             else:
                 j = i
                 target = 0 # use target to store each sum
-                
+
                 # use while loop to make sure j go backwards of each element for the summation until index 0 (inclusive)
                 while j >= 0:
                     target += nums[j] # add each element to target
@@ -20,4 +36,5 @@ class Solution:
                 sum_list.append(target) # add target to sum_list
 
         return sum_list
+    """
                     
